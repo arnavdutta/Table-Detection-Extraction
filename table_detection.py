c2c768edfe666443eeb37862f0d89fa40b88b8eb
arnavdutta <39775516+arnavdutta@users.noreply.github.com>
2019-08-06 00:08:08 +0530

    Code Update

--- table_detection.py
+++ table_detection.py
@@ -9,35 +9,36 @@ def table_detection(img_path):
     (thresh, img_bin) = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     img_bin = cv2.bitwise_not(img_bin)
 
-    kernel_length_h = (np.array(img_gray).shape[1])//40
     kernel_length_v = (np.array(img_gray).shape[1])//120
-    
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length_v))    
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length_h, 1))
-    
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length_v)) 
     im_temp1 = cv2.erode(img_bin, vertical_kernel, iterations=3)
     vertical_lines_img = cv2.dilate(im_temp1, vertical_kernel, iterations=3)
 
+    kernel_length_h = (np.array(img_gray).shape[1])//40
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length_h, 1))
     im_temp2 = cv2.erode(img_bin, horizontal_kernel, iterations=3)
     horizontal_lines_img = cv2.dilate(im_temp2, horizontal_kernel, iterations=3)
 
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     table_segment = cv2.addWeighted(vertical_lines_img, 0.5, horizontal_lines_img, 0.5, 0.0)
     table_segment = cv2.erode(cv2.bitwise_not(table_segment), kernel, iterations=2)
     thresh, table_segment = cv2.threshold(table_segment, 0, 255, cv2.THRESH_OTSU)
-
-    cv2.imwrite("./results/table_detect/table_detect__" + img_path.split('/')[-1], table_segment)
-    
-    im, contours, hierarchy = cv2.findContours(table_segment, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+   
+    _, contours, hierarchy = cv2.findContours(table_segment, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     count = 0
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
         if (w > 80 and h > 20) and w > 3 * h:
             count += 1
             cropped = img[y:y + h, x:x + w]
-            cv2.imwrite("./results/cropped/crop_" + str(count) + "__" + img_path.split('/')[-1], cropped)
+            
+            if True:
+            	cv2.imwrite("./results/cropped/crop_" + str(count) + "__" + img_path.split('/')[-1], cropped)
         cv2.rectangle(img,(x, y),(x + w, y + h),(0, 255, 0), 2)
-    cv2.imwrite("./results/bb/bb__" + img_path.split('/')[-1], img)
+
+    if True:    
+	    cv2.imwrite("./results/table_detect/table_detect__" + img_path.split('/')[-1], table_segment)
+	    cv2.imwrite("./results/bb/bb__" + img_path.split('/')[-1], img)
 
 
 {table_detection('./forms/' + i) for i in os.listdir('./forms/') if i.endswith('.png') or i.endswith('.PNG') or i.endswith('.jpg') or i.endswith('.JPG')}
